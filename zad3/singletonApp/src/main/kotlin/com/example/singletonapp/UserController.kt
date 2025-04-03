622ea019d57a77e745ac68e1a495d4462e15f944
JustynaGargula <s221391@student.uek.krakow.pl>
2025-04-03 17:35:36 +0200

    addded login function

--- zad3/singletonApp/src/main/kotlin/com/example/singletonapp/UserController.kt
+++ zad3/singletonApp/src/main/kotlin/com/example/singletonapp/UserController.kt
@@ -2,20 +2,31 @@ package com.example.singletonapp
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping("/users")
-class UserControler {
+@RequestMapping
+class UserController {
 
-    @GetMapping
+    @GetMapping("/users")
     fun getUsers(): List<User>{
         return listOf<User>(
             User("user1", "pass1"),
             User("user2", "pass2"),
             User("user3", "pass3")
         )
+    }
+
+    @GetMapping("/login")
+    fun login(@RequestParam userName: String, @RequestParam password: String): String{
+        if(AuthService.authenticate(userName, password)) {
+            return "Successfully logged in"
+        }
+        else{
+            return "Log in failed"
+        }
     }
 
 }
