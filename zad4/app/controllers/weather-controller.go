11d28a6c3c54656d20a1aacdbd81e400ca403cd2
JustynaGargula <s221391@student.uek.krakow.pl>
2025-04-10 16:37:10 +0200

    Created weather model

--- zad4/app/controllers/weather-controller.go
+++ zad4/app/controllers/weather-controller.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
+type Weather struct {
+	gorm.Model
+	City string
+	Date string
+	Temperature string
+}
+
+var weatherData = []Weather {
+	{City: "Cracow", Date: "10.04.2025", Temperature: "5"},
+	{City: "Cracow", Date: "11.04.2025", Temperature: "10"},
+}
 
 func GetWeatherForecast(c echo.Context) error {
 	
-	return c.String(http.StatusOK, "Hopefully, it's going to be pretty nice")
+	return c.JSON(http.StatusOK, weatherData)
 }
